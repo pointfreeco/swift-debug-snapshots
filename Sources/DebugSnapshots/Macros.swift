8611faef797f6b840545d7fc14ef305664507daa
stephencelis <658+stephencelis@users.noreply.github.com>
2026-08-04 19:57:13 +0000

    Run swift-format

--- Sources/DebugSnapshots/Macros.swift
+++ Sources/DebugSnapshots/Macros.swift
@@ -157,20 +157,24 @@ public macro DebugSnapshotCheck<T: DebugSnapshotConvertible & AnyObject>(_ type:
 
 @_documentation(visibility: private)
 @attached(peer)
-public macro DebugSnapshotCheck<C: Collection where C.Element: AnyObject>(_ type: C.Type) =
+public macro DebugSnapshotCheck<C: Collectionwhere C.Element: AnyObject>(_ type: C.Type) =
   #externalMacro(module: "DebugSnapshotsMacros", type: "DebugSnapshotCheckFailAnyObjectMacro")
 
 @_documentation(visibility: private)
 @attached(peer)
-public macro DebugSnapshotCheck<C: Collection where C.Element: AnyObject>(_ type: C?.Type) =
+public macro DebugSnapshotCheck<C: Collectionwhere C.Element: AnyObject>(_ type: C?.Type) =
   #externalMacro(module: "DebugSnapshotsMacros", type: "DebugSnapshotCheckFailAnyObjectMacro")
 
 @_documentation(visibility: private)
 @attached(peer)
-public macro DebugSnapshotCheck<C: Collection & DebugSnapshotConvertible where C.Element: AnyObject>(_ type: C.Type) =
+public macro DebugSnapshotCheck<C: Collection & DebugSnapshotConvertiblewhere C.Element: AnyObject>(
+  _ type: C.Type
+) =
   #externalMacro(module: "DebugSnapshotsMacros", type: "DebugSnapshotCheckFailConvertibleMacro")
 
 @_documentation(visibility: private)
 @attached(peer)
-public macro DebugSnapshotCheck<C: Collection & DebugSnapshotConvertible where C.Element: AnyObject>(_ type: C?.Type) =
+public macro DebugSnapshotCheck<C: Collection & DebugSnapshotConvertiblewhere C.Element: AnyObject>(
+  _ type: C?.Type
+) =
   #externalMacro(module: "DebugSnapshotsMacros", type: "DebugSnapshotCheckFailConvertibleMacro")
